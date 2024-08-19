81dca1ff59cd4dd14136df49e2f62fb8f47bb76c
AK-Developers <aryankhatri9811@gmail.com>
2024-08-19 15:31:43 +0530

    first

---
+++ backend/routes/models/config.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///medixify.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
